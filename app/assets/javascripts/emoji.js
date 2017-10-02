088c51e7964a6baa92661995346e50c491378ee5
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-10-01 19:45:19 -0600

    Added Emoji quick search

--- app/assets/javascripts/emoji.js
+++ app/assets/javascripts/emoji.js
@@ -1,49 +1,63 @@
-// $(document).ready(function() {
-//
-//   var emoji_data, emojiNames = [], emojiAliases = []
-//   setTimeout(function() {
-//     $.getJSON("/emoji.json", function(data) {
-//       emoji_data = data
-//       for (var emojiName in emoji_data) {
-//         emojiNames.push(emojiName)
-//         emojiAliases = emojiAliases.concat(emoji_data[emojiName])
-//       }
-//       emojify()
-//     })
-//   }, 1)
-//
-//   emoji = function(icon) {
-//     icon = icon.replace(":", "").toLowerCase()
-//
-//     if (emojiNames.indexOf(icon) < 0) {
-//       // Doesn't exist, return the original string
-//       return ":" + icon + ":"
-//     }
-//
-//     var new_emoji = $("#pre-load .emoji").clone()
-//     new_emoji.attr("alt", ":" + icon + ":")
-//     new_emoji.attr("title", ":" + icon + ":")
-//     new_emoji.removeClass("blank").addClass(icon)
-//
-//     return $("<span>").html(new_emoji).get(0).innerHTML
-//   }
-//
-//   emojify = function(selector) {
-//     selector = selector || "body"
-//     var not_between_carrots_regex = /[^<>]+(?![^<]*>)/g
-//     var emoji_regex = /([^a-zA-Z0-9\\]?)\:([^ \n]+?)\:/g
-//
-//     $(selector).each(function() {
-//       this.innerHTML = this.innerHTML.replace(not_between_carrots_regex, function(match) {
-//         return match.replace(emoji_regex, function(emoji_match, group1, group2) {
-//           if (group1) {
-//             return group1 + emoji(group2)
-//           } else {
-//             return emoji(group2)
-//           }
-//         })
-//       })
-//     })
-//   }
-//
-// })
+$(document).ready(function() {
+
+  addEmojiToLoader = function(name, aliases) {
+    var colon_name = ":" + name + ":"
+    var list_names = [name].concat(aliases).join(" ")
+    var emoji = $("<i>", { alt: colon_name, title: colon_name, class: "emoji large " + name })
+    var emoji_name = $("<div>", { class: "emoji-name" }).html(colon_name)
+    var emoji_wrapper = $("<div>", { class: "emoji-wrapper" }).append(emoji, emoji_name)
+    var alias_list = $("<span>", { class: "aliases" }).html(aliases.map(function(alias) { return ":" + alias + ":" }).join(", "))
+    var emoji_container = $("<div>", { class: "emoji-container", "data-names": list_names }).append(emoji_wrapper, alias_list)
+    $(".emoji-loader").append(emoji_container)
+  }
+
+  var emoji_data, emojiNames = [], emojiAliases = []
+  setTimeout(function() {
+    $.getJSON("/emoji.json", function(data) {
+      emoji_data = data
+      for (var emojiName in emoji_data) {
+        emojiNames.push(emojiName)
+        emojiAliases = emojiAliases.concat(emoji_data[emojiName])
+        addEmojiToLoader(emojiName, emoji_data[emojiName])
+      }
+    })
+  }, 10)
+
+  $(".emoji-quick-search").on("keyup", function() {
+    var search_text = $(this).val().toLowerCase()
+
+    if (search_text.length == 0) {
+      $(".emoji-container").removeClass("hidden")
+    }
+
+    $(".emoji-container").each(function() {
+      var names = $(this).attr("data-names").split(" ")
+
+      var hasMatched = false
+      for (var name_idx in names) {
+        if (hasMatched) { break }
+
+        var name = names[name_idx], emojiText = name, string_valid = true
+
+        for (var idx in search_text.split("")) {
+          if (hasMatched) { break }
+          var char = search_text[idx], char_index = emojiText.indexOf(char)
+
+          if (char_index < 0) {
+            string_valid = false
+          } else {
+            emojiText = emojiText.substr(char_index + 1)
+          }
+        }
+
+        if (string_valid) { hasMatched = true }
+      }
+      if (hasMatched) {
+        $(this).removeClass("hidden")
+      } else {
+        $(this).addClass("hidden")
+      }
+    })
+  })
+
+})
