21b4aa03dc5bc91ed9c863c19f3f24abd776c1aa
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-10-02 00:16:18 -0600

    WIP Emoji Popup

--- app/assets/javascripts/emoji.js
+++ app/assets/javascripts/emoji.js
@@ -4,9 +4,9 @@ $(document).ready(function() {
     var colon_name = ":" + name + ":"
     var list_names = [name].concat(aliases).join(" ")
     var emoji = $("<i>", { alt: colon_name, title: colon_name, class: "emoji " + name })
-    var emoji_name = $("<div>", { class: "emoji-name" }).html(colon_name)
+    var emoji_name = $("<div>", { class: "emoji-name" }).html(name)
     var emoji_wrapper = $("<div>", { class: "emoji-wrapper" }).append(emoji, emoji_name)
-    var alias_list = $("<span>", { class: "aliases" }).html(aliases.map(function(alias) { return ":" + alias + ":" }).join(", "))
+    var alias_list = $("<span>", { class: "aliases" }).html(aliases.join(", "))
     var emoji_container = $("<div>", { class: "emoji-container", "data-names": list_names }).append(emoji_wrapper, alias_list)
     $(".emoji-loader").append(emoji_container)
   }
@@ -14,7 +14,7 @@ $(document).ready(function() {
   var emoji_data, emojiNames = [], emojiAliases = []
   setTimeout(function() {
     if ($(".emoji-field").length > 0) {
-      $("#pre-load").append($("<div>", { class: "emoji-loader" }))
+      $("body").append($("<div>", { class: "emoji-loader small field-autofiller hidden" }))
     }
     $.getJSON("/emoji.json", function(data) {
       emoji_data = data
@@ -23,13 +23,13 @@ $(document).ready(function() {
         emojiAliases = emojiAliases.concat(emoji_data[emojiName])
         addEmojiToLoader(emojiName, emoji_data[emojiName])
         if ($(".emoji-loader").hasClass("large")) { $(".emoji").addClass("large") }
+        // if ($(".emoji-loader").hasClass("small")) { $(".emoji").addClass("small") }
       }
     })
   }, 10)
 
-  $(".emoji-quick-search, .emoji-field").on("keyup", function() {
-    var search_text = $(this).val().toLowerCase().replace(/[ \_\-]/, "")
-
+  filterEmojiFromText = function(search_text) {
+    search_text = search_text.toLowerCase().replace(/[ \_\-\:]/, "")
     if (search_text.length == 0) {
       $(".emoji-container").removeClass("hidden")
     }
@@ -42,7 +42,7 @@ $(document).ready(function() {
         if (hasMatched) { break }
 
         var name = names[name_idx], emojiText = name, string_valid = true
-        emojiText = emojiText.replace(/[ \_\-]/, "")
+        emojiText = emojiText.replace(/[ \_\-\:]/, "")
 
         // NOTE: Word-based matching
         if (emojiText.indexOf(search_text) < 0) { string_valid = false }
@@ -67,11 +67,77 @@ $(document).ready(function() {
         $(this).addClass("hidden")
       }
     })
+  }
+
+  $(".emoji-quick-search").on("keyup", function() {
+    var search_text = $(this).val()
+
+    filterEmojiFromText(search_text)
+  })
+
+  caretPositionInField = function(text_field) {
+    var caretPos = 0
+    if (document.selection) {
+      text_field.focus()
+      var selection = document.selection.createRange()
+      selection.moveStart('character', -text_field.value.length)
+      caretPos = selection.text.length
+    } else if (text_field.selectionStart || text_field.selectionStart == '0') {
+      caretPos = text_field.selectionStart
+    }
+    return caretPos
+  }
+
+  wordFromPosition = function(text, caretPos) {
+    var index = text.indexOf(caretPos)
+    var preText = text.substring(0, caretPos)
+    if (preText.indexOf(" ") > 0) {
+      var words = preText.split(" ")
+      return words[words.length - 1]
+    } else {
+      return preText
+    }
+  }
+
+  currentWordForField = function(field) {
+    var pos = caretPositionInField(field)
+    return wordFromPosition(field.value, pos)
+  }
+
+  alignAutoFillerToFocusedField = function() {
+    var focusedField = $(document.activeElement)
+    if (!focusedField.hasClass("emoji-field")) { return }
+    var fieldPos = focusedField.position()
+    var popup = $(".field-autofiller")
+    var popupHeight = popup.height()
+    popup.css({ top: fieldPos.top - popupHeight, left: fieldPos.left, width: focusedField.outerWidth() })
+  }
+
+  $(window).scroll(function() {
+    if (!$(".field-autofiller").hasClass("hidden")) {
+      alignAutoFillerToFocusedField()
+    }
+  })
+
+  $(".emoji-container").click(function() {
+    // replace current word with emoji syntax
+    // Hide popup
+    // move cursor to end of emoji?
   })
 
   $(".emoji-field").on("keyup", function() {
-    // Find the word the cursor is currently typing.
-    // If the word starts with a colon and matches the regex, perform a filter
+    // TODO: Interrupt all non character events while popup is shown (Enter, Esc, Tab, arrow keys)
+    //  Navigate currently shown emojis with those keys instead
+    var currentWord = currentWordForField(this)
+    if (currentWord[0] == ":" && currentWord[currentWord.length - 1] != ":") {
+      filterEmojiFromText(currentWord)
+      $(".field-autofiller").removeClass("hidden")
+    } else {
+      $(".field-autofiller").addClass("hidden")
+    }
+    alignAutoFillerToFocusedField()
+  }).on("blur", function() {
+    $(".field-autofiller").addClass("hidden")
   })
 
 })
