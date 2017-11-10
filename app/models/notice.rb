20d58cdf29cf5e5a26364ef33b2af6221d2eda79
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-11-09 18:15:23 -0700

    Fixed message

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -89,7 +89,7 @@ class Notice < ApplicationRecord
         return if previous_notification > 30.minutes.ago
       end
     end
-    UserMailer.notifications(user, notice_message(root_domain)).deliver_later
+    UserMailer.notifications(user, notice_message(passed_root: root_domain)).deliver_later
   end
 
 end
