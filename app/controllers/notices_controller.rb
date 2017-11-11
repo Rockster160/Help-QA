303617cc1c66faff8f6b889829d9d9331d6eeb96
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-11-11 13:52:36 -0700

    notices shouldn't get read BEFORE displaying

--- app/controllers/notices_controller.rb
+++ app/controllers/notices_controller.rb
@@ -4,9 +4,9 @@ class NoticesController < ApplicationController
   def index
     all_notices = current_user.notices.order(created_at: :desc)
     @notices = all_notices.read.page(params[:page])
-    @unread = all_notices.unread
-    @unread.by_type(:other, :friend_request).each(&:read)
-    @unread = @unread.group_by(&:groupable_identifier)
+    unread_notices = all_notices.unread
+    @unread = unread_notices.group_by(&:groupable_identifier)
+    unread_notices.by_type(:other, :friend_request).each(&:read)
   end
 
 end
