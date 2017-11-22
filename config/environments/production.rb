cffbd6edb469c29c1decb1d9ae8097d3347a54a5
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-11-22 10:23:31 -0700

    Force SSL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
+  config.force_ssl = true
 
   config.action_mailer.default_url_options = { host: 'help-qa.com' }
   config.action_mailer.raise_delivery_errors = false
