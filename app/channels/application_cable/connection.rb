5243d784a59e592cb30a5fbd5d46aaa43d772be8
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-11-23 12:17:53 -0700

    Hopefully fixed issue where Chat ips were getting mixed up

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -11,6 +11,8 @@ module ApplicationCable
 
     def find_verified_user # this checks whether a user is authenticated with devise
       if verified_user = env['warden'].user
+        Sherlock.acting_user = verified_user
+        Sherlock.acting_ip = verified_user.try(:super_ip) || request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
         verified_user
       end
     end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -145,7 +145,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_ip_address
-    request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
+    current_user.try(:super_ip) || request.env['HTTP_X_REAL_IP'] || request.env['REMOTE_ADDR']
   end
 
 end
