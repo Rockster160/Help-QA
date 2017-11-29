f33e1e87f41a4820dfba010c3b22b4386b8d2ab4
Rocco Nicholls <rocco11nicholls@gmail.com>
2017-11-28 19:33:31 -0700

    Fixed dropdowns not disappearing on mobile

--- app/assets/javascripts/ui_popups.js
+++ app/assets/javascripts/ui_popups.js
@@ -1,17 +1,22 @@
 $(document).on("mouseenter", ".show-tooltip", function() {
-  $(this).find(".tooltip").removeClass("hidden");
+  $(this).find(".tooltip").removeClass("hidden")
 }).on("mouseleave", ".show-tooltip", function() {
-  $(this).find(".tooltip").addClass("hidden");
-});
+  $(this).find(".tooltip").addClass("hidden")
+})
 
 $(document).on("mouseenter", ".dropdown-clickable", function(evt) {
-  $(this).siblings(".dropdown-list").removeClass("hidden");
-}).on("click tap touch touchstart", ".dropdown-clickable", function(evt) {
-  if ($(this).siblings(".dropdown-list").hasClass("hidden")) {
-    evt.preventDefault();
-    $(this).siblings(".dropdown-list").removeClass("hidden");
-    return false;
+  $(this).siblings(".dropdown-list").removeClass("hidden")
+}).on("click tap touch touchstart", function(evt) {
+  var $originalTarget = $(evt.originalEvent.target)
+  if ($originalTarget.hasClass("dropdown-container") || $originalTarget.parents(".dropdown-container").length > 0) {
+    if ($originalTarget.siblings(".dropdown-list").hasClass("hidden")) {
+      evt.preventDefault()
+      $originalTarget.siblings(".dropdown-list").removeClass("hidden")
+      return false
+    }
+  } else {
+    $(".dropdown-list").addClass("hidden")
   }
 }).on("mouseleave", ".dropdown-container", function() {
-  $(".dropdown-list").addClass("hidden");
-});
+  $(".dropdown-list").addClass("hidden")
+})
