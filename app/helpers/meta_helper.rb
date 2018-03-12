563c36876d7a8344c1459cb9d573e69fc9143a8c
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-03-12 13:28:15 -0600

    Fix injection in title

--- app/helpers/meta_helper.rb
+++ app/helpers/meta_helper.rb
@@ -1,10 +1,10 @@
 module MetaHelper
   def meta_title(str, include_name: true)
-    content_for(:title) { str.html_safe }
+    content_for(:title) { CGI.escapeHTML(str).html_safe }
   end
 
   def meta_description(description)
-    content_for(:description) { description.html_safe }
+    content_for(:description) { CGI.escapeHTML(description).html_safe }
   end
 
   def meta_no_index
