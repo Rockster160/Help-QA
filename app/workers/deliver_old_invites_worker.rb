e0c288080068426f07c4802dc150c4b438c556ef
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-03-23 23:44:30 -0600

    increased weight

--- app/workers/deliver_old_invites_worker.rb
+++ app/workers/deliver_old_invites_worker.rb
@@ -5,7 +5,7 @@ class DeliverOldInvitesWorker
   def perform
     File.open("contact").read.split("\n").each do |email|
       puts "Delivering to: #{email}"
-      # UserMailer.historical_invite(email).deliver
+      UserMailer.historical_invite(email).deliver
     end
   end
 end
