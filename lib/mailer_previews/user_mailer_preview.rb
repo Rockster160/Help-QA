49fe51d8623845f0ada075d33154e9d1a10db786
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-03-23 22:28:51 -0600

    Styled up the email

--- lib/mailer_previews/user_mailer_preview.rb
+++ lib/mailer_previews/user_mailer_preview.rb
@@ -2,4 +2,8 @@ class UserMailerPreview < ActionMailer::Preview
   def notifications
     UserMailer.notifications(User.not_helpbot.first, "Some generic message")
   end
+
+  def historical_invite
+    UserMailer.historical_invite("some@email.com")
+  end
 end
