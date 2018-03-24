4af84751c43b4c46b7240c124e7fdc492a16ce6c
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-03-23 22:13:18 -0600

    Added mailer and a few tweaks to the invite

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -20,6 +20,13 @@ class UserMailer < ApplicationMailer
       template_name: user.unconfirmed_email.present? ? 'reconfirmation_instructions' : 'confirmation_instructions'
     })
   end
+
+  def historical_invite(email)
+    mail({
+      to: email,
+      subject: "Help.com needs you!"
+    })
+  end
 end
 
 # ActionMailer::Base.mail(from: "\"Help-QA\" <helpqacontact@gmail.com>", to: "rocco11nicholls+test@gmail.com", subject: "test", body: "test").deliver
