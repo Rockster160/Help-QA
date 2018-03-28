d94908c0b111cc7b4e490a0d1b743d90d25d4b40
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-03-27 20:34:52 -0600

    Better urls

--- app/models/email_blob.rb
+++ app/models/email_blob.rb
@@ -17,7 +17,10 @@
 #
 
 class EmailBlob < ApplicationRecord
+  include UrlHelper
   after_commit { delay(:parse) if created_at == updated_at }
+
+  validates :blob, presence: true
 
   def json; @json ||= JSON.parse(blob); end
   def message; @message ||= JSON.parse(json["Message"]); end
@@ -43,17 +46,17 @@ class EmailBlob < ApplicationRecord
     self.from = message.dig("mail", "commonHeaders", "from").join(",")
     self.to = message.dig("mail", "commonHeaders", "to").join(",")
     self.date = message.dig("mail", "commonHeaders", "date")
-    self.text = messages.first.gsub(/<.*?>/, "").gsub(/(\r\n\>\=20)+/, "\n").squish
+    self.text = messages.first.gsub(/<.*?>/, "").gsub(/(\r\n\>\=20)+/, "\n").gsub("Content-Transfer-Encoding: quoted-printable Content-Type: text/plain; charset=UTF-8 ", "").squish
     self.spam = header_from_content("X-SES-Spam-Verdict") != "PASS"
     self.virus = header_from_content("X-SES-Virus-Verdict") != "PASS"
     save
-    path = Rails.application.routes.url_helpers.admin_email_blob_path(self)
+    path = url_for(Rails.application.routes.url_helpers.admin_email_blob_path(self))
     SlackNotifier.notify("New Email from #{from}\n<#{path}|Click here to view.>" , channel: '#helpqa', username: 'Help-Bot', icon_emoji: ':mailbox:')
   end
 
   def to_html
     html = messages.join('<hr style="border-color: blue; border-size: 10px;">')
-    html.gsub(/(\r\n\>\=20)+/, "<br>").gsub("\r\n", "<br>")
+    html.gsub(/(\r\n\>\=20)+/, "<br>").gsub("\r\n", "<br>").gsub(/\<\/?script/, "XscriptX")
   end
 
   private
