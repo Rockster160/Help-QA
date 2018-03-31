004652beed1c3bdf2083f14b0379c89b25ddae05
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-03-31 12:01:18 -0600

    Better display

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -10,7 +10,7 @@ class TagsController < ApplicationController
     @tags = Tag.by_words(params[:tags].split(","))
     return render :no_tag if @tags.none?
     @posts = Post.not_closed.displayable.by_tags(@tags.pluck(:tag_name)).order(created_at: :desc, id: :desc)
-    @users = User.displayable.by_tags(@tags.pluck(:tag_name)).order(last_seen_at: :desc, id: :desc)
+    @users = User.displayable.verified.by_tags(@tags.pluck(:tag_name)).order(last_seen_at: :desc, id: :desc)
   end
 
   def redirect
