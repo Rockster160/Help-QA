a4d06bc538820066d0a7586417d08397920864ec
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-04-01 10:15:18 -0600

    Helpbot check

--- app/models/help_bot.rb
+++ app/models/help_bot.rb
@@ -29,7 +29,7 @@ class HelpBot
       return if user.nil?
       url = url_for(route_for(:account_settings_path))
       user.settings.update(send_email_notifications: false)
-      return if helpbot.shouts.shouts_from.where(sent_to: user).any?
+      return if helpbot.shouts_from.where(sent_to: user).any? # This should check only the bad emails - maybe just add a boolean to see if that email has been attempted already?
       shout_user(user, "Hi there!\n\nI just tried to send you an email, but was told it doesn't exist. Could you verify that you've typed your email correctly? You can view your email and change it by visiting this url:\n\n#{url}\n\nIf you don't want to receive email notifications, that's okay! It will be helpful to use a real email account so you can access your account if you forget your password. You can opt-out of all other emails by clicking \"Do not email me\" in your account settings on the same page you set your email.")
     end
 
