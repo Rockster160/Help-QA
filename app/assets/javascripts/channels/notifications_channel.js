954d7fd8ec35fbfb43cd9e7394eff18c1d6c8b68
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-04-08 23:39:09 -0600

    Added a small loading indicator for when the site is still connecting

--- app/assets/javascripts/channels/notifications_channel.js
+++ app/assets/javascripts/channels/notifications_channel.js
@@ -6,9 +6,12 @@ $(document).ready(function() {
     channel_id: "notifications_" + current_userid
   }, {
     connected: function() {
+      $("#loading-notifications").addClass("hidden")
       updateNotifications()
     },
-    disconnected: function() {},
+    disconnected: function() {
+      $("#loading-notifications").removeClass("hidden")
+    },
     received: function(data) {
       if (data["message"] != undefined && data["message"].length > 0) {
         addFlashNotice(data["message"])
