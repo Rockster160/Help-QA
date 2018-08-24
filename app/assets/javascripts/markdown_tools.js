a80e82d1c9bb44074aebce5c5d7722a258338ab3
Rocco Nicholls <rocco11nicholls@gmail.com>
2018-08-24 17:25:39 -0600

    Basic tools added

--- app/assets/javascripts/markdown_tools.js
+++ app/assets/javascripts/markdown_tools.js
@@ -1,13 +1,3 @@
-function repositionPopups() {
-  $(".markdown-tools:not(.hidden)").each(function() {
-    var toolWrapper = $(this)
-    var textarea = toolWrapper.siblings("textarea")
-    var fieldPos = textarea.offset()
-    var popupHeight = toolWrapper.height()
-    toolWrapper.css({ top: fieldPos.top - popupHeight - 7, left: fieldPos.left, width: textarea.outerWidth() })
-  })
-}
-
 function insertAtCursor(field, insertBefore, insertAfter) {
   if (document.selection) {
     field.focus()
@@ -23,27 +13,19 @@ function insertAtCursor(field, insertBefore, insertAfter) {
   }
 }
 
-function insertAroundCursor(field, insertAround) {
-  insertAtCursor(field, insertAround, insertAround)
-}
-
-$(document).on("focus", "textarea", function() {
-}).on("click", ".tool-cell", function(evt) {
+$(document).on("click", ".tool-cell", function(evt) {
   var field = $(this).parents(".markdown-tools").siblings("textarea").get(0)
-  console.log(field);
   evt.preventDefault()
-  insertAroundCursor(field, "*")
+  insertAtCursor(field, $(this).attr("data-markdown-open").replace("\\n", "\n"), $(this).attr("data-markdown-close").replace("\\n", "\n"))
   return false
-}).on("blur", "textarea", function(evt) {
-  if ($(evt.target).closest(".markdown-tools").length > 0) {
-    evt.preventDefault()
-    return false
-  } else {
-  }
-}).on("click", ".tools-toggle-button", function() {
-  $(this).siblings(".markdown-tools").toggleClass("hidden")
-  repositionPopups()
+}).on("click", ".open-tools", function() {
+  var tools = $(this).siblings(".markdown-tools")
+  var field = $(this).siblings("textarea")
+  tools.removeClass("hidden")
+  field.css("padding-top", tools.outerHeight())
+}).on("click", ".close-tools", function() {
+  var tools = $(this).parents(".markdown-tools")
+  var field = tools.siblings("textarea")
+  field.css("padding-top", "0")
+  tools.addClass("hidden")
 })
-// $(".markdown-tools").addClass("hidden")
-
-$(window).on("scroll resize", repositionPopups)
