103dcfaa3471936530ec20780d83d17c7075980d
Rocco Nicholls <rocco11nicholls@gmail.com>
2019-02-19 10:52:04 -0700

    Alert me when new posts are made

--- app/models/help_bot.rb
+++ app/models/help_bot.rb
@@ -21,6 +21,21 @@ class HelpBot
 
     def react_to_post(post)
       return if Rails.env.archive?
+      # Notify me
+      author = post.author
+      author_path = url_for(Rails.application.routes.url_helpers.user_path(author))
+      post_path = url_for(Rails.application.routes.url_helpers.post_path(post))
+
+      slack_message = "Author: #{'un' unless author.verified?}verified user <@#{author_path}|#{author.username}>\nPost: <#{post_path}|#{post.title}>"
+
+      attachment = {
+        fallback: slack_message,
+        title: "New post has been submitted",
+        text: slack_message,
+        color: "#0F6FFF"
+      }
+      SlackNotifier.notify("", attachments: [attachment])
+      # ^^
       return unless Tag.sounds_depressed?(post.body)
 
       reply_to_post(post, ApplicationController.render(partial: "replies/helpbot_message"))
@@ -53,7 +68,7 @@ class HelpBot
         first_reply = failed_user.replies.first
         if first_reply.sounds_like_spam?
           first_reply.ignore_sherlock = true
-          first_reply.destroy 
+          first_reply.destroy
         end
       end
     end
