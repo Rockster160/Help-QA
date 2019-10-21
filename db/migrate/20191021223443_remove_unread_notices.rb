884f9f92c41611e96fd92a0c812900a2cee0d26c
Rocco Nicholls <rocco11nicholls@gmail.com>
2019-10-21 16:41:38 -0600

    Fixed typo

--- db/migrate/20191021223443_remove_unread_notices.rb
+++ db/migrate/20191021223443_remove_unread_notices.rb
@@ -1,6 +1,6 @@
 class RemoveUnreadNotices < ActiveRecord::Migration[5.0]
   def change
-    Notice.joins(:post).where.not(posts: { closed_at: nil }).where(notices: { read_at: nil }).each(&:read!)
-    Notice.joins(:post).where.not(posts: { removed_at: nil }).where(notices: { read_at: nil }).each(&:read!)
+    Notice.joins(:post).where.not(posts: { closed_at: nil }).where(notices: { read_at: nil }).each(&:read)
+    Notice.joins(:post).where.not(posts: { removed_at: nil }).where(notices: { read_at: nil }).each(&:read)
   end
 end
