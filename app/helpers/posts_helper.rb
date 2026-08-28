41819cb2b6b0d20db8f092c1d0d1e5c2169f40f6
Rocco Nicholls <rocco11nicholls@gmail.com>
2026-08-27 18:19:50 -0600

    Fix

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -10,7 +10,7 @@ module PostsHelper
 
     tags.map do |tag|
       size = range_map(tag.tags_count, min_tag_count, max_tag_count, min, max)
-      tag_href = URI.unescape(href).gsub("{{tag}}", tag.tag_name.to_s)
+      tag_href = URI::DEFAULT_PARSER.unescape(href).gsub("{{tag}}", tag.tag_name.to_s)
       "<a href=\"#{tag_href}\" rel=\"nofollow\" class=\"underline\" style=\"font-size: #{size}px;\" title=\"#{pluralize(tag.tags_count, "post")}\">#{tag.tag_name}</a>"
     end.join(", ").html_safe
   end
